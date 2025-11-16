febd25617723cc5ec6b257a2d5f97b809dd0808b
Travis Gockel <travis@gockelhut.com>
2025-11-16 09:47:09 -0700

    fix: Do not require `windows-x86_64` to be enabled on Windows AArch64
    On Windows AArch64, the x86-64 binary is used as the fallback `etcd`
    binary. This required users to explicitly enable the feature
    `windows-x86_64` to work. This changes the implementation to not require
    that.

--- etcd-bin-vendored/src/lib.rs
+++ etcd-bin-vendored/src/lib.rs
@@ -1,6 +1,6 @@
 #![doc = include_str!("../README.md")]
 
-use std::{env, error, fmt, path::Path};
+use std::{error, fmt, path::Path};
 
 /// Error returned when `etcd` is not supported on the requested OS and architecture.
 pub struct ArchitectureNotSupported {
@@ -8,21 +8,13 @@ pub struct ArchitectureNotSupported {
 }
 
 enum ArchitectureNotSupportedInner {
-    Unknown {
-        os: &'static str,
-        arch: &'static str,
-    },
-    #[allow(dead_code, reason = "this is only used when a feature is disabled")]
-    Disabled(Crate),
+    Unknown { os: &'static str, arch: &'static str },
 }
 
 impl fmt::Display for ArchitectureNotSupported {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self.inner {
             ArchitectureNotSupportedInner::Unknown { os, arch } => write!(f, "etcd not supported for {os}-{arch}"),
-            ArchitectureNotSupportedInner::Disabled(feature_name) => {
-                write!(f, "etcd supported, but the {feature_name} feature is not enabled")
-            }
         }
     }
 }
@@ -35,51 +27,11 @@ impl fmt::Debug for ArchitectureNotSupported {
                 .field("os", &os)
                 .field("arch", &arch)
                 .finish(),
-            ArchitectureNotSupportedInner::Disabled(crate_name) => f
-                .debug_tuple("ArchitectureNotSupported::Disabled")
-                .field(&crate_name)
-                .finish(),
         }
     }
 }
 
 impl error::Error for ArchitectureNotSupported {}
-
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
-enum Crate {
-    LinuxAMD64,
-    LinuxARM64,
-    LinuxPPC64LE,
-    MacAMD64,
-    WindowsAMD64,
-}
-
-impl fmt::Display for Crate {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            Self::LinuxAMD64 => write!(f, "linux-amd64"),
-            Self::LinuxARM64 => write!(f, "linux-arm64"),
-            Self::LinuxPPC64LE => write!(f, "linux-ppc64le"),
-            Self::MacAMD64 => write!(f, "macos-amd64"),
-            Self::WindowsAMD64 => write!(f, "windows-amd64"),
-        }
-    }
-}
-
-impl Crate {
-    pub fn detect() -> Result<Self, ArchitectureNotSupported> {
-        match (env::consts::OS, env::consts::ARCH) {
-            ("linux", "x86_64") => Ok(Self::LinuxAMD64),
-            ("linux", "aarch64") => Ok(Self::LinuxARM64),
-            ("linux", "powerpc64") => Ok(Self::LinuxPPC64LE),
-            ("macos", "x86_64") => Ok(Self::MacAMD64),
-            ("windows", "x86_64" | "aarch64") => Ok(Self::WindowsAMD64),
-            (os, arch) => Err(ArchitectureNotSupported {
-                inner: ArchitectureNotSupportedInner::Unknown { os, arch },
-            }),
-        }
-    }
-}
 
 macro_rules! match_platform {
     (
@@ -97,11 +49,22 @@ macro_rules! match_platform {
                 #[cfg(not(any(all(target_os = $os, target_arch = $arch), feature = $feature)))]
                 $(#[$attr])*
                 Crate::$variant => Err(ArchitectureNotSupported {
-                    inner: ArchitectureNotSupportedInner::Disabled($detected)
+                    inner: ArchitectureNotSupportedInner::Disabled { go_arch: $detected, feature: $feature }
                 }),
             )*
         }
     };
+    ($(($os:literal, $arch:literal, $feature:literal, $module:ident)),* $(,)?) => {
+        match (::std::env::consts::OS, ::std::env::consts::ARCH) {
+            $(
+                #[cfg(any(all(target_os = $os, target_arch = $arch), feature = $feature))]
+                ($os, $arch) => $module::etcd_bin_path().map_err(|_| unreachable!()),
+            )*
+            (os, arch) => Err(ArchitectureNotSupported {
+                inner: ArchitectureNotSupportedInner::Unknown { os, arch }
+            })
+        }
+    }
 }
 
 /// Return a path to the `etcd` binary for this platform.
@@ -109,15 +72,12 @@ macro_rules! match_platform {
 /// This function returns the path to the `etcd` program when it is supported on the current platform. In the case of an
 /// unsupported platform, an `Err(ArchitectureNotSupported)` is returned with a description of the error.
 pub fn etcd_bin_path() -> Result<&'static Path, ArchitectureNotSupported> {
-    let detected = Crate::detect()?;
-
-    match_platform!(detected;
-        (LinuxAMD64,    "linux",    "x86_64",    "linux-x86_64",    etcd_bin_vendored_linux_amd64),
-        (LinuxARM64,    "linux",    "aarch64",   "linux-aarch64",   etcd_bin_vendored_linux_arm64),
-        (LinuxPPC64LE,  "linux",    "powerpc64", "linux-powerpc64", etcd_bin_vendored_linux_ppc64le),
-        (MacAMD64,      "macos",    "x86_64",    "macos-x86_64",    etcd_bin_vendored_darwin_amd64),
-        (WindowsAMD64,  "windows",  "x86_64",    "windows-x86_64",  etcd_bin_vendored_windows_amd64),
-        #[allow(unreachable_patterns)]
-        (WindowsAMD64,  "windows",  "aarch64",   "windows-x86_64",  etcd_bin_vendored_windows_amd64),
-    )
+    match_platform! {
+        ("linux",    "x86_64",    "linux-x86_64",    etcd_bin_vendored_linux_amd64),
+        ("linux",    "aarch64",   "linux-aarch64",   etcd_bin_vendored_linux_arm64),
+        ("linux",    "powerpc64", "linux-powerpc64", etcd_bin_vendored_linux_ppc64le),
+        ("macos",    "x86_64",    "macos-x86_64",    etcd_bin_vendored_darwin_amd64),
+        ("windows",  "x86_64",    "windows-x86_64",  etcd_bin_vendored_windows_amd64),
+        ("windows",  "aarch64",   "windows-x86_64",  etcd_bin_vendored_windows_amd64),
+    }
 }
